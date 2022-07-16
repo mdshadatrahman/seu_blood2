e22554e0f4ae2bf9251df8cd1d1c6adf7f394c7c
Shadat Rahman <shadat.rahman.464@gmail.com>
2022-07-16 21:24:31 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:seu_blood_2/pages/splash/splash_screen.dart';
+import 'package:seu_blood_2/utils/app_colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SEU Blood Bank',
+      theme: ThemeData(
+        primaryColor: ColorManager.primaryColor,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
